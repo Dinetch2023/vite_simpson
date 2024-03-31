f25c8c5b74c1cbabee66c73a8ad7a3b6a262e555
ETCHART Amandine <amandine.yann.lola@gmail.com>
2024-03-31 16:54:00 +0200

    👌

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
 import Avatar from "./components/Avatar";
 function App() {
-  const bart = {
-    image:
-      "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
-    firstName: "Bart",
-    lastName: "Simpson",
-    donuts: 5,
-  };
-  const homer = {
-    image:
-      "https://www.stickees.com/files/cartoon/the-simpsons/2248-homer-simpson-happy.png",
-    firstName: "Homer",
-    lastName: "Simpson",
-    donuts: 45,
-  };
-  const marge = {
-    image:
-      "https://www.stickees.com/files/cartoon/the-simpsons/2252-marge-simpson-sticker.png",
+  const personnages = [
+    {
+      image:
+        "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
+      firstName: "Bart",
+      lastName: "Simpson",
+      donuts: 5,
+    },
+    {
+      image:
+        "https://www.stickees.com/files/cartoon/the-simpsons/2248-homer-simpson-happy.png",
+      firstName: "Homer",
+      lastName: "Simpson",
+      donuts: 45,
+    },
+    {
+      image:
+        "https://www.stickees.com/files/cartoon/the-simpsons/2252-marge-simpson-sticker.png",
 
-    firstName: "Marge",
-    lastName: "Simpson",
-    donuts: -10,
-  };
-  const lisa = {
-    image:
-      "https://www.stickees.com/files/cartoon/the-simpsons/2250-lisa-simpson-sticker.png",
-    firstName: "Lisa",
-    lastName: "Simpson",
-    donuts: 2,
-  };
-  const maggie = {
-    image:
-      "https://www.stickees.com/files/cartoon/the-simpsons/2251-maggie-simpson-sticker.png",
-    firstName: "Maggie",
-    lastName: "Simpson",
-  };
+      firstName: "Marge",
+      lastName: "Simpson",
+      donuts: -10,
+    },
+    {
+      image:
+        "https://www.stickees.com/files/cartoon/the-simpsons/2250-lisa-simpson-sticker.png",
+      firstName: "Lisa",
+      lastName: "Simpson",
+      donuts: 2,
+    },
+    {
+      image:
+        "https://www.stickees.com/files/cartoon/the-simpsons/2251-maggie-simpson-sticker.png",
+      firstName: "Maggie",
+      lastName: "Simpson",
+    },
+  ];
 
   return (
     <>
